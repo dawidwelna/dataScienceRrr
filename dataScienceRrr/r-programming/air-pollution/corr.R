dd1dd59255f8dd1879f57980d526000ce1f75414
Dawid Wełna <jan125djw@gmail.com>
2017-10-10 17:22:54 +0200

    assignment2

--- dataScienceRrr/r-programming/air-pollution/corr.R
+++ dataScienceRrr/r-programming/air-pollution/corr.R
@@ -1,17 +1,17 @@
-complete <- function(directory, ids = 1:332){
-  
-  data <- list.files(directory, full.names = TRUE)
-  
-  nobs <- vector(mode = "integer", length(ids))
-  for(i in seq_along(ids)){
-    
-    x <- read.csv(data[ids[i]])
-    nobs[i] <- sum( complete.cases(x) )
-  }
-  
-  
-  data.frame(ids, nobs)
-}
+# complete <- function(directory, ids = 1:332){
+#   
+#   data <- list.files(directory, full.names = TRUE)
+#   
+#   nobs <- vector(mode = "integer", length(ids))
+#   for(i in seq_along(ids)){
+#     
+#     x <- read.csv(data[ids[i]])
+#     nobs[i] <- sum( complete.cases(x) )
+#   }
+#   
+#   
+#   data.frame(ids, nobs)
+# }
 
 corr <- function(directory, threshold = 0){
   
